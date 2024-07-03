5270b2d55f8919c32f854471ba4dc0e93c72b1f9
EmeRus <rusen.emela14@gmail.com>
2024-07-03 14:39:32 +0300

    Bishop moves

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,27 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves: Square[] = [];
+        let currentSquare = board.findPiece(this);
+
+        const directions = [
+            { row: 1, col: 1 },   // Top-right
+            { row: 1, col: -1 },  // Top-left
+            { row: -1, col: 1 },  // Bottom-right
+            { row: -1, col: -1 }  // Bottom-left
+        ];
+
+        for (const direction of directions) {
+            let row : number = currentSquare.row + direction.row;
+            let col : number = currentSquare.col + direction.col;
+            while (row >= 0 && row < 8 && col >= 0 && col < 8) {
+                const square = Square.at(row, col);
+                moves.push(square);
+                row += direction.row;
+                col += direction.col;
+            }
+        }
+
+        return moves;
     }
 }
