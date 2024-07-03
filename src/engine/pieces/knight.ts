ed5492f4c141f0a8b30ba6b00ecd499ecc74cddf
EmeRus <rusen.emela14@gmail.com>
2024-07-03 15:03:29 +0300

    Knight moves

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,28 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves : Square[] = [];
+        let currentSquare = board.findPiece(this);
+
+        const directions = [
+            { row: 2, col: 1 },
+            { row: 2, col: -1 },
+            { row: -2, col: 1 },
+            { row: -2, col: -1 },
+            { row: 1, col: 2 },
+            { row: 1, col: -2 },
+            { row: -1, col: 2 },
+            { row: -1, col: -2 }
+        ];
+
+        for (const direction of directions) {
+            let row : number = currentSquare.row + direction.row;
+            let col : number = currentSquare.col + direction.col;
+            const square = Square.at(row, col);
+            moves.push(square);
+
+        }
+        return moves;
+
     }
 }
