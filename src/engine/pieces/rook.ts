f3b0d4cf8dbf073135298919687cd0d7e8bd632d
EmeRus <rusen.emela14@gmail.com>
2024-07-03 14:23:51 +0300

    Rook moves

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,22 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves : Square[] = [];
+        let currentSquare = board.findPiece(this);
+
+        for(let col=0; col<8; col++){
+            if(col!=currentSquare.col){
+                let square = Square.at(currentSquare.row, col);
+                moves.push(square);
+            }
+        }
+
+        for(let row=0; row<8; row++){
+            if(row!=currentSquare.row){
+                let square = Square.at(row, currentSquare.col);
+                moves.push(square);
+            }
+        }
+        return moves;
     }
 }
