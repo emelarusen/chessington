5b7b97f3f543cdb0fc98a61152d2a3676a512bc1
EmeRus <rusen.emela14@gmail.com>
2024-07-03 15:11:33 +0300

    King moves

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,28 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves : Square[] = [];
+        let currentSquare = board.findPiece(this);
+
+        const directions = [
+            { row: 1, col: -1}, //up-left
+            { row: 1, col: 0 }, //up
+            { row: 1, col: 1 }, //up-right
+            { row: 0, col: 1 }, //right
+            { row: -1, col: 1 }, //down-right
+            { row: -1, col: 0 }, //down
+            { row: -1, col: -1 }, //down-left
+            { row: 0, col: -1 } //left
+        ];
+
+        for (const direction of directions) {
+            let row : number = currentSquare.row + direction.row;
+            let col : number = currentSquare.col + direction.col;
+            const square = Square.at(row, col);
+            moves.push(square);
+
+        }
+
+        return moves;
     }
 }
