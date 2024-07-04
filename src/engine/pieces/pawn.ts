1889b2d9ced87b159db579aca2dcda0370f7082e
EmeRus <rusen.emela14@gmail.com>
2024-07-04 10:17:30 +0300

    Pawns can take pieces

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,7 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
-import Square from '../square'
+import Square from '../square';
+import King from "./king";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -11,27 +12,34 @@ export default class Pawn extends Piece {
     public getAvailableMoves(board: Board) {
         let moves: Square[] = [];
         let currentSquare = board.findPiece(this);
-
         let direction = (this.player === Player.WHITE) ? 1 : -1;
 
-        let SquareAhead = Square.at(currentSquare.row + direction, currentSquare.col);
+        let squareAhead = Square.at(currentSquare.row + direction, currentSquare.col);
+        if (squareAhead.row >= 0 && squareAhead.row < 8 && !board.getPiece(squareAhead)) {
+            moves.push(squareAhead);
 
-        if( (SquareAhead.row<7 && this.player === Player.WHITE)
-            ||  (SquareAhead.row>0 && this.player === Player.BLACK)) {
-            if (board.getPiece(SquareAhead) === undefined) {
-                moves.push(SquareAhead);
+            if ((this.player === Player.WHITE && currentSquare.row === 1) ||
+                (this.player === Player.BLACK && currentSquare.row === 6)) {
+                let twoSquaresAhead = Square.at(currentSquare.row + 2 * direction, currentSquare.col);
+                if (!board.getPiece(twoSquaresAhead)) {
+                    moves.push(twoSquaresAhead);
+                }
             }
         }
 
-        if( (board.findPiece(this).row === 1 && this.player === Player.WHITE)
-            ||  (board.findPiece(this).row === 6 && this.player === Player.BLACK)){
+        // Capture
+        const captureMoves = [
+            Square.at(currentSquare.row + direction, currentSquare.col - 1),
+            Square.at(currentSquare.row + direction, currentSquare.col + 1)
+        ];
 
-            let twoSquaresAhead = Square.at(SquareAhead.row + direction, SquareAhead.col);
-
-            if (board.getPiece(twoSquaresAhead) === undefined && board.getPiece(SquareAhead) === undefined) {
-                moves.push(twoSquaresAhead);
+        for (const captureMove of captureMoves) {
+            if (captureMove.row >= 0 && captureMove.row < 8 && captureMove.col >= 0 && captureMove.col < 8) {
+                const blockingPiece = board.getPiece(captureMove);
+                if (blockingPiece && blockingPiece.player !== this.player && !(blockingPiece instanceof King)) {
+                    moves.push(captureMove);
+                }
             }
-
         }
 
         return moves;
