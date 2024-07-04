6e427c744169b3e956cda1d9b86a71ea537c5b5a
EmeRus <rusen.emela14@gmail.com>
2024-07-04 09:49:04 +0300

    Rook moves correctly

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from '../square';
+import King from "./king";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -9,24 +10,37 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        let moves : Square[] = [];
-        let currentSquare = board.findPiece(this);
+        const moves: Square[] = [];
+        const currentSquare = board.findPiece(this);
 
-        for(let col=0; col<8; col++){
-            if(col!=currentSquare.col){
-                let square = Square.at(currentSquare.row, col);
-                if(board.getPiece(square)!=undefined) break;
+        const directions = [
+            { row: 0, col: 1 },  // Right
+            { row: 0, col: -1 }, // Left
+            { row: 1, col: 0 },  // Down
+            { row: -1, col: 0 }  // Up
+        ];
+
+        for (const direction of directions) {
+            let row = currentSquare.row + direction.row;
+            let col = currentSquare.col + direction.col;
+
+            while (row >= 0 && row < 8 && col >= 0 && col < 8) {
+                const square = Square.at(row, col);
+                const blockingPiece = board.getPiece(square);
+
+                if (blockingPiece) {
+                    if (blockingPiece.player !== this.player && !(blockingPiece instanceof King)) {
+                        moves.push(square);
+                    }
+                    break;
+                }
+
                 moves.push(square);
+                row += direction.row;
+                col += direction.col;
             }
         }
 
-        for(let row=0; row<8; row++){
-            if(row!=currentSquare.row){
-                let square = Square.at(row, currentSquare.col);
-                if(board.getPiece(square)!=undefined) break;
-                moves.push(square);
-            }
-        }
         return moves;
     }
 }
